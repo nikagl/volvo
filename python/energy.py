255ca2cb4e83f550e02071df7c59491e9d1ee96f
Nika Gerson Lohman <nikagl@gmail.com>
2023-02-13 09:14:45 +0100

    Additional debugging

--- python/energy.py
+++ python/energy.py
@@ -1,76 +1,80 @@
 import requests
 import json
-from os.path import join, dirname, expanduser
-from itertools import product
 
-def read_config():
-    """Read config from file."""
+print ("Connecting...")
 
-    # iterate folders and files
-    for directory, filename in product(
-        [
-            ".",
-            expanduser("~"),
-        ],
-        ["volvo.conf", ".volvo.conf"],
-    ):
-        try:
-            # open folder / file
-            config = join(directory, filename)
-            with open(config) as config:
-                # return the dictionary with parameters from config file
-                return dict(
-                    x.split(": ")
-                    for x in config.read().strip().splitlines()
-                    if not x.startswith("#")
-                )
-        except (IOError, OSError):
-            # try next folder / file
-            continue
-    return {}
-
-config = read_config()
-
-username = config["volvoid_username"]
-password = config["volvoid_password"]
-vccapikey = config["vcc_apikey"]
-vehicle = config["volvo_vin"]
+username = '<USER>'
+password = '<PW>'
+vccapikey = '<VCCAPIKEY>'
 
 try:
-    authentication = requests.post(
+    response = requests.post(
         "https://volvoid.eu.volvocars.com/as/token.oauth2",
         headers = {
             'authorization': 'Basic aDRZZjBiOlU4WWtTYlZsNnh3c2c1WVFxWmZyZ1ZtSWFEcGhPc3kxUENhVXNpY1F0bzNUUjVrd2FKc2U0QVpkZ2ZJZmNMeXc=',
             'content-type': 'application/x-www-form-urlencoded',
             'user-agent': 'okhttp/4.10.0'
         },
-        data = {  
-            'username': config["volvoid_username"],
-            'password': config["volvoid_password"],
+        data = {
+            'username': username,
+            'password': password,
             'access_token_manager_id': 'JWTh4Yf0b',
             'grant_type': 'password',
             'scope': 'openid email profile care_by_volvo:financial_information:invoice:read care_by_volvo:financial_information:payment_method care_by_volvo:subscription:read customer:attributes customer:attributes:write order:attributes vehicle:attributes tsp_customer_api:all conve:brake_status conve:climatization_start_stop conve:command_accessibility conve:commands conve:diagnostics_engine_status conve:diagnostics_workshop conve:doors_status conve:engine_status conve:environment conve:fuel_status conve:honk_flash conve:lock conve:lock_status conve:navigation conve:odometer_status conve:trip_statistics conve:tyre_status conve:unlock conve:vehicle_relation conve:warnings conve:windows_status energy:battery_charge_level energy:charging_connection_status energy:charging_system_status energy:electric_range energy:estimated_charging_time energy:recharge_status vehicle:attributes'
         }
     )
-    print("Authentication successful!")
-    access_token = authentication.json()['access_token']
+    # print(json.dumps(response.json(), indent=4))
+    print("Login successful!")
+    print(response.json())
 except requests.exceptions.RequestException as error:
-    print("Authentication failed: {}".format(error))
+    print("Login failed:")
+    print(error)
+
+access_token = response.json()['access_token']
+# print(access_token)
 
 try:
-    url = "https://api.volvocars.com/energy/v1/vehicles/" + config["volvo_vin"] + '/recharge-status'
-    print("URL: {}".format(url))
     vehicles = requests.get(
-        url,
+        "https://api.volvocars.com/extended-vehicle/v1/vehicles",
         headers= {
-            "accept": "application/vnd.volvocars.api.energy.vehicledata.v1+json",
-            "vcc-api-key": config["vcc_apikey"],
+            "accept": "application/json",
+            "vcc-api-key": vccapikey,
             "Authorization": "Bearer " + access_token
         }
     )
-    print("Result: {}".format(vehicles))
+    print("\nResult:")
+    print(vehicles)
+    vjson=vehicles.json()
+    print("Vin = "+str(vjson["vehicles"][0]["id"]))
+
     vehiclesjson = json.dumps(vehicles.json(), indent=4)
-    print("\nResult JSON: {}".format(vehiclesjson))
+    print("\nResult JSON:")
+    print(vehiclesjson)
+
 except requests.exceptions.RequestException as error:
     print("Get vehicles failed:")
+    print(error)
+
+vin = vjson["vehicles"][0]["id"]
+
+try:
+    status = requests.get(
+        "https://api.volvocars.com/energy/v1/vehicles/"+vin+"/recharge-status",
+        headers= {
+            "accept": "application/vnd.volvocars.api.energy.vehicledata.v1+json",
+            "vcc-api-key": vccapikey,
+            "Authorization": "Bearer " + access_token
+        }
+    )
+
+    print("\nResult:")
+    print(status)
+    sjson=status.json()
+
+    sjson = json.dumps(status.json(), indent=4)
+    print("\nResult JSON:")
+    print(sjson)
+
+except requests.exceptions.RequestException as error:
+    print("Get rechargestatus failed:")
     print(error)
--- python/volvo-test.py
+++ python/volvo-test.py
@@ -47,8 +47,8 @@ try:
         data = {  
             'username': config["volvoid_username"],
             'password': config["volvoid_password"],
-            'access_token_manager_id': 'JWTh4Yf0b',
             'grant_type': 'password',
+            'access_token_manager_id': 'JWTh4Yf0b',
             'scope': 'openid email profile care_by_volvo:financial_information:invoice:read care_by_volvo:financial_information:payment_method care_by_volvo:subscription:read customer:attributes customer:attributes:write order:attributes vehicle:attributes tsp_customer_api:all conve:brake_status conve:climatization_start_stop conve:command_accessibility conve:commands conve:diagnostics_engine_status conve:diagnostics_workshop conve:doors_status conve:engine_status conve:environment conve:fuel_status conve:honk_flash conve:lock conve:lock_status conve:navigation conve:odometer_status conve:trip_statistics conve:tyre_status conve:unlock conve:vehicle_relation conve:warnings conve:windows_status energy:battery_charge_level energy:charging_connection_status energy:charging_system_status energy:electric_range energy:estimated_charging_time energy:recharge_status vehicle:attributes'
         }
     )
@@ -58,19 +58,21 @@ except requests.exceptions.RequestException as error:
     print("Authentication failed: {}".format(error))
 
 try:
-    url = "https://api.volvocars.com/energy/v1/vehicles/" + config["volvo_vin"] + '/recharge-status'
+    url = "https://api.volvocars.com/extended-vehicle/v1/vehicles/" + config["volvo_vin"] + "/resources"
     print("URL: {}".format(url))
-    vehicles = requests.get(
+    print("apikey: {}".format(config["vcc_apikey"]))
+    vehicledata = requests.get(
         url,
         headers= {
-            "accept": "application/vnd.volvocars.api.energy.vehicledata.v1+json",
+            "accept": "application/json",
+#            "accept": "application/vnd.volvocars.api.connected-vehicle.vehicledata.v1+json",
             "vcc-api-key": config["vcc_apikey"],
             "Authorization": "Bearer " + access_token
         }
     )
-    print("Result: {}".format(vehicles))
-    vehiclesjson = json.dumps(vehicles.json(), indent=4)
-    print("\nResult JSON: {}".format(vehiclesjson))
+    print("Result: {}".format(vehicledata))
+    vehicledatajson = json.dumps(vehicledata.json(), indent=4)
+    print("\njson:\n{}".format(vehicledatajson))
 except requests.exceptions.RequestException as error:
     print("Get vehicles failed:")
     print(error)
